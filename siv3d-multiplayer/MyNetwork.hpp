822b61e5803621db36e92426202463523ed91e2a
Tamagosushio <mitachi.ts75@gmail.com>
2025-06-19 13:53:08 +0900

    [add] ○×ゲームルールを追加

---
+++ siv3d-multiplayer/MyNetwork.hpp
@@ -0,0 +1,191 @@
+﻿# pragma once
+
+# include "Multiplayer_Photon.hpp"
+# include "PHOTON_APP_ID.SECRET"
+
+class MyNetwork : public Multiplayer_Photon {
+public:
+  static constexpr int32 MaxPlayers = 3;
+  using Multiplayer_Photon::Multiplayer_Photon;
+private:
+  Array<LocalPlayer> m_localPlayers;
+
+  void connectReturn(
+    [[maybe_unused]] const int32 errorCode,
+    const String& errorString,
+    const String& region,
+    [[maybe_unused]] const String& cluster
+  ) override {
+    if (m_verbose) {
+      Print << U"MyNetwork::connectReturn() [サーバへの接続を試みた結果を処理する]";
+    }
+    if (errorCode) {
+      if (m_verbose) {
+        Print << U"[サーバへの接続に失敗] " << errorString;
+      }
+      return;
+    }
+    if (m_verbose) {
+      Print << U"[サーバへの接続に成功]";
+      Print << U"[region: {}]"_fmt(region);
+      Print << U"[ユーザ名: {}]"_fmt(getUserName());
+      Print << U"[ユーザ ID: {}]"_fmt(getUserID());
+    }
+    Scene::SetBackground(ColorF{ 0.4, 0.5, 0.6 });
+  }
+
+  void disconnectReturn() override {
+    if (m_verbose) {
+      Print << U"MyNetwork::disconnectReturn() [サーバから切断したときに呼ばれる]";
+    }
+    m_localPlayers.clear();
+    Scene::SetBackground(Palette::DefaultBackground);
+  }
+
+  void joinRandomRoomReturn(
+    [[maybe_unused]] const LocalPlayerID playerID,
+    const int32 errorCode,
+    const String& errorString
+  ) override {
+    if (m_verbose) {
+      Print << U"MyNetwork::joinRandomRoomReturn() [既存のランダムなルームに参加を試みた結果を処理する]";
+    }
+    if (errorCode == NoRandomMatchFound) {
+      const RoomName roomName = (getUserName() + U"'s room-" + ToHex(RandomUint32()));
+      if (m_verbose) {
+        Print << U"[参加可能なランダムなルームが見つからなかった]";
+        Print << U"[自分でルーム " << roomName << U" を新規作成する]";
+      }
+      createRoom(roomName, MaxPlayers);
+      return;
+    }
+    else if (errorCode) {
+      if (m_verbose) {
+        Print << U"[既存のランダムなルームへの参加でエラーが発生] " << errorString;
+      }
+      return;
+    }
+    if (m_verbose) {
+      Print << U"[既存のランダムなルームに参加できた]";
+    }
+  }
+
+  void createRoomReturn(
+    [[maybe_unused]] const LocalPlayerID playerID,
+    const int32 errorCode,
+    const String& errorString
+  ) override {
+    if (m_verbose) {
+      Print << U"MyNetwork::createRoomReturn() [ルームを新規作成した結果を処理する]";
+    }
+    if (errorCode) {
+      if (m_verbose) {
+        Print << U"[ルームの新規作成でエラーが発生] " << errorString;
+      }
+      return;
+    }
+    if (m_verbose) {
+      Print << U"[ルーム " << getCurrentRoomName() << U" の作成に成功]";
+    }
+  }
+
+  void joinRoomEventAction(
+    const LocalPlayer& newPlayer,
+    [[maybe_unused]] const Array<LocalPlayerID>& playerIDs,
+    const bool isSelf
+  ) override {
+    if (m_verbose) {
+      Print << U"MyNetwork::joinRoomEventAction() [誰か（自分を含む）が現在のルームに参加したときに呼ばれる]";
+    }
+    m_localPlayers = getLocalPlayers();
+    if (m_verbose) {
+      Print << U"[{} (ID: {}) がルームに参加した。ローカル ID: {}] {}"_fmt(newPlayer.userName, newPlayer.userID, newPlayer.localID, (isSelf ? U"(自分自身)" : U""));
+      Print << U"現在の " << getCurrentRoomName() << U" のルームメンバー";
+      for (const auto& player : m_localPlayers) {
+        Print << U"- [{}] {} (id: {}) {}"_fmt(player.localID, player.userName, player.userID, player.isHost ? U"(host)" : U"");
+      }
+    }
+  }
+
+  void leaveRoomEventAction(
+    const LocalPlayerID playerID,
+    [[maybe_unused]] const bool isInactive
+  ) override {
+    if (m_verbose) {
+      Print << U"MyNetwork::leaveRoomEventAction() [誰かがルームから退出したら呼ばれる]";
+    }
+    m_localPlayers = getLocalPlayers();
+    if (m_verbose) {
+      for (const auto& player : m_localPlayers) {
+        if (player.localID == playerID) {
+          Print << U"[{} (ID: {}, ローカル ID: {}) がルームから退出した]"_fmt(player.userName, player.userID, player.localID);
+        }
+      }
+      Print << U"現在の " << getCurrentRoomName() << U" のルームメンバー";
+      for (const auto& player : m_localPlayers) {
+        Print << U"- [{}] {} (ID: {}) {}"_fmt(player.localID, player.userName, player.userID, player.isHost ? U"(host)" : U"");
+      }
+    }
+  }
+
+  void leaveRoomReturn(
+    int32 errorCode,
+    const String& errorString
+  ) override {
+    if (m_verbose) {
+      Print << U"MyNetwork::leaveRoomReturn() [ルームから退出したときに呼ばれる]";
+    }
+    m_localPlayers.clear();
+    if (errorCode) {
+      if (m_verbose) {
+        Print << U"[ルームからの退出でエラーが発生] " << errorString;
+      }
+      return;
+    }
+  }
+
+  // int32 を受信したときに呼ばれる関数をオーバーライドしてカスタマイズする
+  void customEventAction(
+    const LocalPlayerID playerID,
+    const uint8 eventCode,
+    const int32 data
+  ) override {
+    Print << U"<<< [" << playerID << U"] からの eventCode: " << eventCode << U", data: int32(" << data << U") を受信";
+  }
+
+  // String を受信したときに呼ばれる関数をオーバーライドしてカスタマイズする
+  void customEventAction(
+    const LocalPlayerID playerID,
+    const uint8 eventCode,
+    const String& data
+  ) override {
+    Print << U"<<< [" << playerID << U"] からの eventCode: " << eventCode << U", data: String(" << data << U") を受信";
+  }
+
+  // Point を受信したときに呼ばれる関数をオーバーライドしてカスタマイズする
+  void customEventAction(
+    const LocalPlayerID playerID,
+    const uint8 eventCode,
+    const Point& data
+  ) override {
+    Print << U"<<< [" << playerID << U"] からの eventCode: " << eventCode << U", data: Point" << data << U" を受信";
+  }
+
+  // Array<int32> を受信したときに呼ばれる関数をオーバーライドしてカスタマイズする
+  void customEventAction(
+    const LocalPlayerID playerID,
+    const uint8 eventCode,
+    const Array<int32>& data
+  ) override {
+    Print << U"<<< [" << playerID << U"] からの eventCode: " << eventCode << U", data: Array<int32>" << data << U" を受信";
+  }
+
+  // Array<String> を受信したときに呼ばれる関数をオーバーライドしてカスタマイズする
+  void customEventAction(
+    const LocalPlayerID playerID,
+    const uint8 eventCode,
+    const Array<String>& data
+  ) override {
+    Print << U"<<< [" << playerID << U"] からの eventCode: " << eventCode << U", data: Array<String>" << data << U" を受信";
+  }
+};
---
+++ siv3d-multiplayer/TicTacToe.hpp
@@ -0,0 +1,157 @@
+﻿# include <Siv3D.hpp>
+
+namespace TikTakToe {
+
+  enum class Cell{
+    None = 0, // 何も置かれていない
+    Circle = 1, // 〇が置かれている
+    Cross = 2 // ×が置かれている
+  };
+
+  struct Operation {
+    Point pos; // 操作される位置
+    Cell cell_type; // 配置するセルの種類
+    Operation() {}
+    Operation(const Point& pos, const Cell cell_type)
+      : pos(pos), cell_type(cell_type) {}
+  };
+
+  class Game {
+  public:
+    Grid<Cell> grid_;
+    size_t cell_size_ = 100;
+    Point cell_offset_{ 100, 100 };
+    Font font_symbol_{ FontMethod::MSDF, static_cast<int32>(cell_size_), Typeface::Bold };
+    Cell player_symbol_;
+    Point get_cell_point_(const size_t y, const size_t x) const;
+    Rect get_cell_rect_(const Point& pos) const;
+    Rect get_cell_rect_(const size_t y, const size_t x) const;
+  public:
+    Game() {}
+    Game(const size_t grid_size, Cell player_simbol);
+    void initialize(const size_t grid_size, Cell player_simbol);
+    void operate(const Operation& op);
+    Cell is_finished(void) const;
+    void draw(void) const;
+    void update(void);
+  };
+  Point Game::get_cell_point_(const size_t y, const size_t x) const {
+    return Point(x * cell_size_, y * cell_size_) + cell_offset_;
+  }
+  Rect Game::get_cell_rect_(const Point& pos) const {
+    return get_cell_rect_(pos.y, pos.x);
+  }
+  Rect Game::get_cell_rect_(const size_t y, const size_t x) const {
+    return Rect{get_cell_point_(y, x), cell_size_};
+  }
+
+  Game::Game(const size_t grid_size, Cell player_symbol) {
+    initialize(grid_size, player_symbol);
+  }
+
+  void Game::initialize(const size_t grid_size, Cell player_symbol) {
+    grid_.clear();
+    grid_.resize(grid_size, grid_size, Cell::None);
+    player_symbol_ = player_symbol;
+  }
+
+  void Game::operate(const Operation& op) {
+    grid_[op.pos] = op.cell_type;
+  }
+
+  Cell Game::is_finished(void) const {
+    Cell winner = Cell::None;
+    Cell cell_type;
+    // 行走査
+    for (size_t col : step(grid_.height())) {
+      // 行が全てNone以外の同じ要素か判定
+      cell_type = grid_[col][0];
+      if (cell_type == Cell::None) continue;
+      bool row_ok = true;
+      for (size_t row : step(grid_.width())) {
+        if (cell_type != grid_[col][row]) {
+          row_ok = false;
+          break;
+        }
+      }
+      if (row_ok) {
+        winner = cell_type;
+      }
+    }
+    // 列走査
+    for (size_t row : step(grid_.width())) {
+      // 列が全てNone以外の同じ要素か判定
+      cell_type = grid_[0][row];
+      if (cell_type == Cell::None) continue;
+      bool col_ok = true;
+      for (size_t col : step(grid_.height())) {
+        if (cell_type != grid_[col][row]) {
+          col_ok = false;
+          break;
+        }
+      }
+      if (col_ok) {
+        winner = cell_type;
+      }
+    }
+    // 対角線走査
+    cell_type = grid_[0][0];
+    if (cell_type != Cell::None) {
+      bool diagonal_ok = true;
+      for (size_t idx = 0; idx < grid_.height(); idx++) {
+        if (cell_type != grid_[idx][idx]) {
+          diagonal_ok = false;
+          break;
+        }
+      }
+      if (diagonal_ok) {
+        winner = cell_type;
+      }
+    }
+    cell_type = grid_[grid_.height()-1][0];
+    if (cell_type != Cell::None) {
+      bool diagonal_ok = true;
+      for (size_t idx = 0; idx < grid_.height(); idx++) {
+        if (cell_type != grid_[grid_.height()-1-idx][idx]) {
+          diagonal_ok = false;
+          break;
+        }
+      }
+      if (diagonal_ok) {
+        winner = cell_type;
+      }
+    }
+    return winner;
+  }
+
+  void Game::update(void) {
+    for (size_t h = 0; h < grid_.height(); h++) {
+      for (size_t w = 0; w < grid_.width(); w++) {
+        if (get_cell_rect_(h, w).leftClicked()) {
+          operate(Operation{
+            Point(w,h), player_symbol_
+          });
+        }
+      }
+    }
+  }
+
+  void Game::draw(void) const {
+    for (size_t h = 0; h < grid_.height(); h++) {
+      for (size_t w = 0; w < grid_.width(); w++) {
+        // グリッドを描画
+        get_cell_rect_(h, w).draw(Palette::Black).drawFrame(5, Palette::White);
+        // セルの種類に応じた描画
+        Cell cell_type = grid_[h][w];
+        if (cell_type != Cell::None) {
+          font_symbol_(cell_type == Cell::Circle ? U"O" : U"X").drawAt(
+            cell_size_ * 0.8,
+            get_cell_point_(h, w) + Point(cell_size_, cell_size_)/2
+          );
+        }
+      }
+    }
+  }
+
+};
+
